e0b74a763075eb9dadd813740340c38f2919fa6f
Emiliano Kohmann <emiliano.kohmann@spoton.com>
2022-09-05 15:08:55 -0300

    Creating serv in controller

--- ej-books/controllers/book/book_controller.go
+++ ej-books/controllers/book/book_controller.go
@@ -1,60 +1,43 @@
 package book
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/emikohmann/ucc-arqsoft-2/ej-books/dtos"
 	service "github.com/emikohmann/ucc-arqsoft-2/ej-books/services"
-	"github.com/emikohmann/ucc-arqsoft-2/ej-books/utils/cache"
+	"github.com/emikohmann/ucc-arqsoft-2/ej-books/services/repositories"
+	"github.com/emikohmann/ucc-arqsoft-2/ej-books/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
+var (
+	serv = service.NewServiceImpl(
+		repositories.NewRepositoryCache(),
+		repositories.NewRepositoryMemcached(),
+		repositories.NewRepositoryMongo(),
+	)
+)
+
 func Get(c *gin.Context) {
-
-	id := c.Param("id")
-
-	res := cache.Get(id)
-
-	if res != "" {
-		var bookDtoCache dtos.BookDto
-		json.Unmarshal([]byte(res), &bookDtoCache)
-		fmt.Println("from cache: " + id)
-		c.JSON(http.StatusOK, bookDtoCache)
-		return
-
-	}
-	bookDto, er := service.BookService.GetBook(id)
-
-	// Error del Insert
-	if er != nil {
-		c.JSON(er.Status(), er)
+	book, apiErr := serv.Get(c.Param("id"))
+	if apiErr != nil {
+		c.JSON(apiErr.Status(), apiErr)
 		return
 	}
-
-	c.JSON(http.StatusOK, bookDto)
+	c.JSON(http.StatusOK, book)
 }
 
 func Insert(c *gin.Context) {
-	var bookDto dtos.BookDto
-	err := c.BindJSON(&bookDto)
-
-	// Error Parsing json param
-	if err != nil {
-		fmt.Println(err)
-		c.JSON(http.StatusBadRequest, err.Error())
+	var book dtos.BookDto
+	if err := c.BindJSON(&book); err != nil {
+		apiErr := errors.NewBadRequestApiError(err.Error())
+		c.JSON(apiErr.Status(), apiErr)
 		return
 	}
 
-	bookDto, er := service.BookService.InsertBook(bookDto)
-
-	// Error del Insert
-	if er != nil {
-		c.JSON(er.Status(), er)
+	book, apiErr := serv.Insert(book)
+	if apiErr != nil {
+		c.JSON(apiErr.Status(), apiErr)
 		return
 	}
-	bookDtoStr, _ := json.Marshal(bookDto)
-	cache.Set(bookDto.Id, bookDtoStr)
-	fmt.Println("save cache: " + bookDto.Id)
-	c.JSON(http.StatusCreated, bookDto)
+	c.JSON(http.StatusCreated, book)
 }
--- ej-books/services/service_impl.go
+++ ej-books/services/service_impl.go
@@ -2,13 +2,26 @@ package services
 
 import (
 	"github.com/emikohmann/ucc-arqsoft-2/ej-books/dtos"
+	"github.com/emikohmann/ucc-arqsoft-2/ej-books/services/repositories"
 	e "github.com/emikohmann/ucc-arqsoft-2/ej-books/utils/errors"
 )
 
-type ServiceImpl struct{}
+type ServiceImpl struct {
+	cache     repositories.RepositoryCache
+	memcached repositories.RepositoryMemcached
+	mongo     repositories.RepositoryMongo
+}
 
-func NewServiceImpl() ServiceImpl {
-	return ServiceImpl{}
+func NewServiceImpl(
+	cache repositories.RepositoryCache,
+	memcached repositories.RepositoryMemcached,
+	mongo repositories.RepositoryMongo,
+) ServiceImpl {
+	return ServiceImpl{
+		cache:     cache,
+		memcached: memcached,
+		mongo:     mongo,
+	}
 }
 
 func (ServiceImpl) Get(id string) (dtos.BookDto, e.ApiError) {
